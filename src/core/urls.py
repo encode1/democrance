7f1d27eb54e8635a86d2ccffd7bc63d12691478e
William Boampong <wk4christ@gmail.com>
2021-11-26 07:06:52 +0000

    Add swagger for documentation

--- src/core/urls.py
+++ src/core/urls.py
@@ -15,11 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from customers.api_v1 import urls as customer_api_v1
 from policy.api_v1 import urls as policy_api_v1
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Democrance API",
+      default_version='v1',
+      description="Democrance API coding test",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@localhost.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('api/v1/', include(customer_api_v1)),
     path('api/v1/', include(policy_api_v1)),
     path('admin/', admin.site.urls),
+    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
